4a408b9d65d730434b123f82656eb4d7daa6a78c
SikHyoKim <dogdog120@naver.com>
2024-01-16 11:20:40 +0900

    랜덤 버튼 생성

--- frontend/src/SearchInput.js
+++ frontend/src/SearchInput.js
@@ -2,15 +2,15 @@ const TEMPLATE = '<input type="text">';
 
 class SearchInput {
   constructor({ $target, onSearch }) {
-    const $searchInput = document.createElement("input");
     const $wrapper = document.createElement("section");
+    $target.appendChild($wrapper);
+    
+    const $searchInput = document.createElement("input");
     this.$searchInput = $searchInput;
     this.$searchInput.placeholder = "고양이를 검색해보세요.|";
 
     $searchInput.className = "SearchInput";
-
     $wrapper.appendChild($searchInput);
-    $target.appendChild($wrapper);
 
     $searchInput.addEventListener("keyup", e => {
       if (e.keyCode === 13) {
@@ -18,7 +18,16 @@ class SearchInput {
       }
     });
 
-    console.log("SearchInput created.", this);
+    const $randomButton = document.createElement('randomButton');
+    this.$randomButton = $randomButton;
+    this.$randomButton.textContent = '랜덤';
+
+    this.$randomButton.className = 'RandomButton';
+    $wrapper.appendChild($randomButton);
+
+    $randomButton.addEventListener("click", e => {
+      onSearch('아');
+    });
   }
   render() {}
 }
